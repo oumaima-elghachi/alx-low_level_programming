59adee4809c96deeae8b1c704325ac8cf80dd14d
oumaima <elghachiomaima@gmail.com>
2023-04-21 00:32:59 +0000

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*this code to test puts commande*/
+/**
+* main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
