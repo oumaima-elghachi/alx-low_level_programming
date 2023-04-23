e35da38c053a94eb01f21d87f8c1fb9beee3273b
oumaima <elghachiomaima@gmail.com>
2023-04-23 23:54:10 +0100

    Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /**
- * this program to print the last digit of n
- * 
- * 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
- 
 int main(void)
 {
 	int n;
