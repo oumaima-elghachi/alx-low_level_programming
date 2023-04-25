af345162c2562346c3e7d36700fecbf43f3cfd30
oumaima <elghachiomaima@gmail.com>
2023-04-25 11:54:06 +0100

    Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,63 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int a;
-	int b;
-	int c;
 
-	for (a = '0' ; a < '9' ; a++)
-	for (b = a + 1; b <= '9'; b++)
-	for (c = b + 1; c <= '9'; c++)
-	{
-		if ((b != a) != c)
-		putchar(a);
-		putchar(b);
-		putchar(c);
-		{
-			if (a == '8' && b == '9')
-			continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+
+/**
+
+ * main - prints all possible different combinations of three digits
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+        int n, m, l;
+
+
+        for (n = 48; n < 58; n++)
+
+        {
+
+                for (m = 49; m < 58; m++)
+
+                {
+
+                        for (l = 50; l < 58; l++)
+
+                        {
+
+                                if (l > m && m > n)
+
+                                {
+
+                                        putchar(n);
+
+                                        putchar(m);
+
+                                        putchar(l);
+
+                                        if (n != 55 || m != 56)
+
+                                        {
+
+                                                putchar(',');
+
+                                                putchar(' ');
+
+                                        }
+
+                                }
+
+                        }
+
+                }
+
+        }
+
+        putchar('\n');
+
+        return (0);
+
 }
