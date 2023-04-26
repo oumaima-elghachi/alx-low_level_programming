ba9a3334559064478931e7f6a59d6e11801f0357
oumaima <elghachiomaima@gmail.com>
2023-04-26 21:40:29 +0100

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - function that checks for lowercase character
+ * @c: The return value.
+ *
+ * Return: 1 for the lowercase and 0 for the capital
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
