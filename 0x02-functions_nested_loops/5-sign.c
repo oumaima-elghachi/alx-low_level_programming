8cdc5eaac4874ffd86b4b0708d75f5aaf5dffad4
oumaima <elghachiomaima@gmail.com>
2023-04-26 22:11:39 +0100

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,12 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
 */
-int print_sign(int n)
+int print_sign(int n) 
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		 return (1);
+		return (1);
 	}
 	else if (n == 0)
 	{
