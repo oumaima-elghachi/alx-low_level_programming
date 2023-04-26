a7c9129eef6d539147d8c879add15bfc3e4b868a
oumaima <elghachiomaima@gmail.com>
2023-04-26 21:49:09 +0100

    Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  *_isalpha - function that checks for lowercase character
  * @c: The return value.
  *
- *Return: 1 for the lowercase and 0 for the capital
+ * Return: 1 for the lowercase and 0 for the capital
  */
-int _isalpha(int c) 
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
