9f4e33841e73ef2f4006dcc3cbebad46a4f0f697
oumaima <elghachiomaima@gmail.com>
2023-04-27 23:54:25 +0100

    debugging

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print_numbers - print the numbers from 0 to 9
- */           
+ * Return: the number from 0 to 9
+ */
 
 void print_numbers(void)
 
@@ -12,7 +13,7 @@ void print_numbers(void)
 
 	for(i = 0; i <= 9; i++)
 	{
-		_putchar(i + 48);
+	_putchar(i + 48);
 	}
 	_putchar('\n');
 }
