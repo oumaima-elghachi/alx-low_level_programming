8973fc8a3d97210072446de955037f3e12f7ee38
oumaima <elghachiomaima@gmail.com>
2023-04-27 23:13:37 +0100

    debugging

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /** 
  *_isupper - uppercase letters
- * @c: int to check
+ * @c: int to check 
  *
  * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
