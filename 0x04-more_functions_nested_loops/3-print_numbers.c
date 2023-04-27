9e05f88624dc2f945553b6831cad6b59d9f8695e
oumaima <elghachiomaima@gmail.com>
2023-04-27 23:51:32 +0100

    debugging

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ */           
+
+void print_numbers(void)
+
+{
+
+	int i;
+
+	for(i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
