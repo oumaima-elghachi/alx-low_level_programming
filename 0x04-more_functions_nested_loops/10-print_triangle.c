287f6c5b1f5a79c3708f4781543634419e6441dd
oumaima <elghachiomaima@gmail.com>
2023-04-28 22:46:21 +0100

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,11 +10,13 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 	{
-		int i, j;
-		
-		for (i = 1; i <= size; i++)
+		int i;
+		int j;
+
+	for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
 			{
