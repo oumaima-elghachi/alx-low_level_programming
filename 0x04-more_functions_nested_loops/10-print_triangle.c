1cf367823395fcf0f17819fe62a11e0941b116f3
oumaima <elghachiomaima@gmail.com>
2023-04-28 11:23:35 +0100

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,31 +3,28 @@
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
- * Return: empty
  */
 
 void print_triangle(int size)
-
 {
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	}else
 	{
 		int i, j;
-
+		
 		for (i = 1; i <= size; i++)
-	{
-		for (j = i; j < size; j++)
-	{
-		_putchar(' ');
-	}
-		for (j = 1; j <= i; j++)
-	}
-		_putchar('#');
-	}
-		_putchar('\n');
-	}
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+		}
 	}
 }
