0a6993dee2f024c1ac2831dadceb27f340dde363
oumaima <elghachiomaima@gmail.com>
2023-05-02 21:01:17 +0100

    pointer,arrays,strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,30 @@
  */
 
 int _atoi(char *s)
-
 {
-
 	int c = 0;
 	unsigned int ni = 0;
 	int min = 1;
-	int isi = 0
+	int isi = 0;
 
 	while (s[c])
 	{
-		if (s[c]) == 45)
-	{
-		min *= -1;
-	}
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
 		while (s[c] >= 48 && s[c] <= 57)
-	{
-		isi = 1;
-	ni = (ni * 10) + (s[c] - '0');
-	c++;
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
 	}
-ni *= min;
+	ni *= min;
 	return (ni);
 }
