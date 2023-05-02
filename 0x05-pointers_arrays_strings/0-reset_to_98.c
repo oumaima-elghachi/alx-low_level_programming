cba1bd2c9ab9f66d68368edef00383cbf416f30e
oumaima <elghachiomaima@gmail.com>
2023-05-02 15:36:43 +0100

    pointer,arrays,strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - taskes a pointer to an int and updates
+ * @n: An integer input pointer
+ * Return : nothing
+ */
+
+void rest_to_98(int *n)
+{
+
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is for pointer arrays and strings project
