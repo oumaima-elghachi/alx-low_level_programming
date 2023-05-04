61d227adc788c379cb4b82a105a00db98a32b659
oumaima <elghachiomaima@gmail.com>
2023-05-04 14:02:13 +0100

    pointer,arrays,strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <='z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
@@ -20,8 +20,8 @@ char *cap_string(char *str)
 		str[index - 1] == ',' ||
 		str[index - 1] == ';' ||
 		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
+	        str[index - 1] == '!' ||
+		str[index - 1] == '?' ||          
 		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
