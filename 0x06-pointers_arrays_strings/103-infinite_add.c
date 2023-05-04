68929b2768814df07eee3918c1cc4dc60544244d
oumaima <elghachiomaima@gmail.com>
2023-05-04 16:47:32 +0100

    pointer,arrays,strings

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Returne: 0
+ */
+
+void rev_string(char *n)
+{
+	int val1 = 0, val2 = 0, temp_tot = 0;
+	while (*(n1 + i) != '\0')
+	i++;
+while (*(n2 + j) != '\0')
+	j++
