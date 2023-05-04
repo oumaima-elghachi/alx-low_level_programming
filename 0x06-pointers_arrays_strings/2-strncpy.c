5d2ede9a64fb9fd609b6cc386da4771a0b9a5a1f
oumaima <elghachiomaima@gmail.com>
2023-05-04 13:13:07 +0100

    pointer,arrays,strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,8 +4,8 @@
  * _strncpy - copy a string
  * @dest: input value
  * @src: input value
- * @n: input value 
- * Return: dest 
+ * @n: input value
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
