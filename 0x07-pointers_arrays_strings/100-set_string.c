7ba415cd8cc13553081b9fa9bd79e39125bc1f3d
oumaima <elghachiomaima@gmail.com>
2023-05-08 13:27:30 +0100

    pointer,arrays,strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * set_string - entry point 
+ * set_string - entry point
  * @s: input
  * @to: input
  * Return: Always 0 (success)
  */
 
 void set_string(char **s, char *to)
-
 {
 	*s = to;
 }
