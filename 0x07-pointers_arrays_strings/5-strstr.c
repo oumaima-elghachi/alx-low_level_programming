32990604125b8a35aa481d796d991e7b692fe011
oumaima <elghachiomaima@gmail.com>
2023-05-08 12:56:21 +0100

    pointer,arrays,strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,17 +2,16 @@
 
 /**
  * _strstr - Entry point
- * @hayastack: input
+ * @haystack: input
  * @needle: input
- * Return: Always 0 (succecc)
+ * Return: Always 0 (success)
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *l = haystack;  
+		char *l = haystack;
 		char *p = needle;
 
 		while (*l == *p && *p != '\0')
@@ -22,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 		}
 
 		if (*p == '\0')
-			return (haystack);
+		return (haystack);
 	}
 	return (0);
 }
